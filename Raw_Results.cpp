072a9f52b5af024dd9c7c245fd178eb9ceff2ef3
theosireix <theo.sireix@gmail.com>
2026-03-27 13:59:34 +0100

    Update Raw_Results.cpp

--- Raw_Results.cpp
+++ Raw_Results.cpp
@@ -1,1 +1,31 @@
+const int pinDigital = 12;
 
+int valeurDigital = 0;
+float mean = 0;
+unsigned long temp1 = 0;
+unsigned long temp2 = 0;
+float delta = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pinDigital, INPUT_PULLUP); // IMPORTANT !
+}
+
+void loop() {
+
+  valeurDigital = digitalRead(pinDigital);
+
+  if (valeurDigital == 1) { 
+    temp2 = millis();
+
+    delta = temp2 - temp1;
+
+    Seial.println(delta);
+
+    // attendre relâchement
+    while (digitalRead(pinDigital) == 1) {}
+
+    temp1 = millis();
+  }
+}
