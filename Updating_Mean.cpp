126308018aee4ac21fde2ed59c113e51ed11b0e6
belgeAttitude <dorian.sen@epfl.ch>
2026-03-27 13:53:19 +0100

    Add mean calculation for digital input timing
    Implement mean calculation based on digital input timing.

---
+++ Updating_Mean.cpp
@@ -0,0 +1,37 @@
+const int pinDigital = 12;
+
+int valeurDigital = 0;
+int i = -1;
+float mean = 0;
+unsigned long temp1 = 0;
+unsigned long temp2 = 0;
+float delta = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pinDigital, INPUT_PULLUP); // IMPORTANT !
+}
+
+void loop() {
+
+  valeurDigital = digitalRead(pinDigital);
+
+  if (valeurDigital == 1) { 
+    temp2 = millis();
+
+    delta = temp2 - temp1;
+
+    i++;
+
+    if (i >= 1 && delta > 0) {
+      mean += ((float)delta - mean) / i;
+      Serial.println(mean);
+    }
+
+    // attendre relâchement
+    while (digitalRead(pinDigital) == 1) {}
+
+    temp1 = millis();
+  }
+}
