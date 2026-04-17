df28b07b21345e1b424ae735b94f2376bb5b2d90
belgeAttitude <dorian.sen@epfl.ch>
2026-04-17 13:20:45 +0200

    Implement infinite loop on 'q' input
    Add infinite loop on receiving 'q' character from Serial.

--- Raw_Results.cpp
+++ Raw_Results.cpp
@@ -6,6 +6,7 @@ unsigned long temp1 = 0;
 unsigned long temp2 = 0;
 float delta = 0;
 float lambda = 0;
+char c;
 
 void setup() {
   Serial.begin(9600);
@@ -27,6 +28,13 @@ void loop() {
     Serial.print(temp2);
     Serial.print(",");
     Serial.println(lambda);
+    char c = Serial.read();
+    if (c == 'q') {
+      Serial.println("Q reçu, boucle infinie !");
+      while (true) {
+        // boucle infinie
+      }
+    }
 
     // attendre relâchement
     while (digitalRead(pinDigital) == 1) {}
